bce5c0a5b73f85dfd5d6e53ab1baabed737214d1
Shiv Pratap <sp09singhwaghel@gmail.com>
2023-08-31 22:31:43 +0530

    app usage to accept the data form the body

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express= require("express");
-const PORT = require("./config/serverConfig");
+
+const {PORT} = require("./config/serverConfig");
+
 const appandport=()=>{ 
     //Setting up the app
     const app=express();
-    let PORT=5000;
+
+    //To accept the response coming form the body
+    app.use(express.json());
+    app.use(express.urlencoded({extended:true}));
+
     //setting up the port
     app.listen(PORT,()=>{
         console.log(`Server is listening on port number ${PORT}`);
